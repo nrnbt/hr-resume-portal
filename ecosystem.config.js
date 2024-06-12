0ab4a40f9de59a5faa6a2352621d11385c4133ed
nrnbt <nrnbt0616@gmail.com>
2024-06-13 01:39:57 +0800

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hr-resume-portal',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      env_development: {
+        NODE_ENV: 'development'
+      }
+    }
+  ]
+};
